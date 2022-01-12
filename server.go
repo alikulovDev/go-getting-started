44098caa023d5dbc7a1558948904f89553a69fb1
azamat aliqulov <azamat.aliqulov@list.ru>
2022-01-12 19:16:20 +0500

    inital

--- server.go
+++ server.go
@@ -2,11 +2,12 @@ package main
 
 import (
 	"fmt"
-	"log"
+	// "log"
 	"net/http"
-
-	"github.com/alikulovDev/go-getting-started/db"
-	"github.com/gorilla/mux"
+	// "log"
+	// "net/http"
+	// "github.com/alikulovDev/go-getting-started/db"
+	// "github.com/gorilla/mux"
 	// "gitlab.com/azamat.aliqulov/go_rest_api/-/blob/main/db.go"
 	// "gitlab.com/azamat.aliqulov/go_rest_api/-/blob/main/quizController.go"
 )
@@ -15,18 +16,18 @@ func helloWorld(w http.ResponseWriter, r *http.Request) {
 	fmt.Printf("Hello world")
 }
 
-func handleRequest() {
-	myRouter := mux.NewRouter().StrictSlash(true)
-	myRouter.HandleFunc("/", helloWorld).Methods("GET")
-	// myRouter.HandleFunc("/users", AllUsers).Methods("GEt")
-	fmt.Println("Hello GO exstensions")
-	fmt.Println("Server at 8082")
-	log.Fatal(http.ListenAndServe(":8082", myRouter))
-}
+// func handleRequest() {
+// 	myRouter := mux.NewRouter().StrictSlash(true)
+// 	myRouter.HandleFunc("/", helloWorld).Methods("GET")
+// 	// myRouter.HandleFunc("/users", AllUsers).Methods("GEt")
+// 	fmt.Println("Hello GO exstensions")
+// 	fmt.Println("Server at 8082")
+// 	log.Fatal(http.ListenAndServe(":8082", myRouter))
+// }
 
 func main() {
 	fmt.Println("Go ORM Tutorial")
-	db.setupDB()
+	// db_cennetion.sayHello()
 	// db_connection()
-	handleRequest()
+	// handleRequest()
 }
